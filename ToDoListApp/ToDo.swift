d61fc04d23d30893e2c371bd2241bf5f8b4fc930
Claire <clairewillliams@gmail.com>
2021-05-12 15:02:13 -0400

    Initial Commit

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Claire Williams on 5/12/21.
+//
+
+import UIKit
+
+class ToDo {
+    var name = " "
+    var important = false
+}
---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Claire Williams on 5/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
